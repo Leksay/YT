8a427caf066f00299af12d1c5ce94eb22597cae8
Aleksei Golubev <>
2023-05-28 13:52:30 +0300

    Added unique receiver id

--- UnityEventBus/Scripts/Interfaces.cs
+++ UnityEventBus/Scripts/Interfaces.cs
@@ -2,7 +2,10 @@
 public interface IEvent{}
 
 // Базовый интерфейс для слушателей событий
-public interface IBaseEventReceiver{}
+public interface IBaseEventReceiver
+{
+    public UniqueId Id { get; }
+}
 
 // Интерфейс для параметризированных слушателей событий
 public interface IEventReceiver<T> : IBaseEventReceiver where T : struct, IEvent
---
+++ UnityEventBus/Scripts/UniqueId.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public record UniqueId
+{
+    public string Id => _id ??= Guid.NewGuid().ToString();
+    private string _id;
+
+    public static implicit operator string(UniqueId uniqueId) => uniqueId.Id;
+}
